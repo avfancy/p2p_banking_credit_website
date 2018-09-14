81774ff3f482850246244a3ca8cba91369dc4a52
avv123 <614277157@qq.com>
2018-09-14 10:09:00 +0800

    更新首页、登录、注册页面

---
+++ login.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>p2p金融借贷网站</title>
+    <!-- 引入bootstrap样式 -->
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+    <!-- 引入自定义less压缩css -->
+    <link rel="stylesheet" href="./dist/css/minCss/regLogin.min.css">
+</head>
+
+<body>
+    <!-- header -->
+    <!-- 引入主导航 -->
+    <?php
+        require_once("./mainNav1.php");
+    ?>
+
+    <!-- nav2 -->
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <img src="./images/logo.png" alt="图片加载失败">
+                </a>
+                <span>用户登录</span>
+            </div>  
+        </div>
+    </nav>
+
+
+    <!-- 模板的内容开始 -->
+    <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">用户登录</h3>
+            </div>
+            <div class="panel-body">
+                <p>请输入用户名和密码</p>
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-3 control-label">用户名</label>
+                        <div class="col-sm-9">
+                        <input type="text" class="form-control" name="username" placeholder="请输入用户名">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-3 control-label">密&emsp;码</label>
+                        <div class="col-sm-9">
+                        <input type="password" class="form-control" name="pwd" placeholder="请输入密码">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-3 col-sm-9">
+                        <button type="submit" class="btn btn-success">登录</button>
+                        <a href="./register.php">新用户，马上注册</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- 模板的内容结尾 -->
+    
+    <!-- 引入页脚文件 -->
+    <?php
+        require_once("footer.php");
+    ?>
+
+
+    <!-- 引入jquery.js -->
+    <script src="./lib/jquery/jquery.min.js"></script>
+    <!-- 引入bootstrap.js -->
+    <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- 引入js -->
+    <script src="./src/js/index.js"></script>
+
+</body>
+
+</html>
---
+++ template.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>p2p金融借贷网站</title>
+    <!-- 引入bootstrap样式 -->
+    <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+    <!-- 引入自定义less压缩css -->
+    <link rel="stylesheet" href="./dist/css/minCss/index.min.css">
+</head>
+
+<body>
+    <!-- 引入头部文件 -->
+    <?php
+        require_once("header.php");
+    ?>
+
+
+    <!-- 模板的内容开始 -->
+    <div class="container">
+        模板内容
+    </div>
+
+    <!-- 模板的内容结尾 -->
+    
+    <!-- 引入页脚文件 -->
+    <?php
+        require_once("footer.php");
+    ?>
+
+
+    <!-- 引入jquery.js -->
+    <script src="./lib/jquery/jquery.min.js"></script>
+    <!-- 引入bootstrap.js -->
+    <script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- 引入js -->
+    <script src="./src/js/index.js"></script>
+
+</body>
+
+</html>
